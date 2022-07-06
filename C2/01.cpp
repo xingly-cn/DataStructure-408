1c5e7a5a4176632689d2426fa20a46b8580586fd
方糖 <xiaoheikeji@vip.qq.com>
2022-07-06 21:51:59 +0800

    c2-01

---
+++ C2/01.cpp
@@ -0,0 +1,46 @@
+//
+// 01.顺序表中删除最小值元素，并返回被删除的元素，空出的元素用最后一个元素填补，顺序表为空抛出异常
+// Created by 方糖 on 2022/7/6.
+//
+#include "bits/stdc++.h"
+using namespace std;
+struct SqList{
+    int data[100];
+    int maxsize,length;
+}sqList;
+
+void add(vector<int> num) {
+    for (auto i : num) {
+        sqList.data[sqList.length++] = i;
+    }
+}
+
+int del_min(SqList &list) {
+    if (list.length == 0) return -1;    // 顺序表为空报错
+    int minValue = sqList.data[0], pos = 0;
+    for (int i = 0 ; i < list.length ; i++) {   // 找到最小值的pos下标
+        if (minValue >= list.data[i]) {
+            minValue = list.data[i];
+            pos = i;
+        }
+    }
+    list.data[pos] = list.data[list.length - 1];    // 覆盖最小值
+    list.length--;  // 不要忘记删除后长度减一
+    return minValue;    // 返回删除的值
+}
+
+int main() {
+
+    // 模拟数据
+    add({5,4,1,2,3});
+
+    cout << "原：";
+    for (int i = 0 ; i < sqList.length ; i++) cout << sqList.data[i] << " ";
+
+    del_min(sqList);
+
+    cout << endl << "删：";
+    for (int i = 0 ; i < sqList.length ; i++) cout << sqList.data[i] << " ";
+
+    return 0;
+}
